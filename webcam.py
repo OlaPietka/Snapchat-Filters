025a85262566886f09a5af79a6ec8e57eb8899f7
Ola Piętka <s17611@pjwstk.edu.pl>
2020-02-13 00:42:45 +0100

    Add new filters and code clean up

--- webcam.py
+++ webcam.py
@@ -7,72 +7,108 @@ CASCADE_DIR = "data/cascades/"
 FILTER_DIR = "data/filters/"
 
 
-def get_image(filter, faces_rects, eyes_rects, frame):
+def menage_filter(filter):
     if filter == "sunglasses":
-        if len(eyes_rects) is not 2:
+        if len(eyes) != 2:
             return
 
-        if eyes_rects[0][0] > eyes_rects[1][0]:
-            ((rx, ry, rw, rh), (lx, ly, lw, lh)) = eyes_rects
-        else:
-            ((lx, ly, lw, lh), (rx, ry, rw, rh)) = eyes_rects
+        (x, y, min_w, min_h, max_w, max_h) = get_boundaries(eyes)
 
-        offset_x = int(lw * 0.6)
-        offset_y = int(lh * 0.3)
+        offset_x = int(min_w * 0.6)
+        offset_y = int(min_h * 0.3)
 
-        glasses = cv2.imread(FILTER_DIR + "sunglasses.png", -1)
-        glasses = cv2.resize(glasses, (rx + rw - lx + offset_x, lh + offset_y))
-        glasses = rotate_image(glasses, np.arctan((ly + lh - ry) / (rx + rw - lx)) * 180 / np.pi - 20)
+        sunglasses = process_image(filter, max_w + offset_x, min_h + offset_y)
+        sunglasses = rotate_image(sunglasses, np.arctan(max_h / max_w) * 180 / np.pi - 20)
 
-        return overlay_image(ly - int(offset_y / 2), lx - int(offset_x / 2), glasses, frame)
+        overlay_image(y - int(offset_y / 2), x - int(offset_x / 2), sunglasses)
     elif filter == "santahat":
-        for (x, y, w, h) in faces_rects:
+        for (x, y, w, h) in faces:
             offset_x = int(w * 0.4)
             offset_y = int(h * 0.2)
 
-            santahat = cv2.imread(FILTER_DIR + "santahat.png", -1)
-            santahat = cv2.resize(santahat, (w + offset_x, h + offset_y))
+            santa_hat = process_image(filter, w + offset_x, h + offset_y)
 
-            frame = overlay_image(y - int(1.8*offset_y), x - int(0.8*offset_x), santahat, frame)
-        return frame
+            overlay_image(y - int(1.8*offset_y), x - int(0.8*offset_x), santa_hat)
     elif filter == "mask":
-        for (x, y, w, h) in faces_rects:
+        for (x, y, w, h) in faces:
             offset_x = int(w * 0.2)
             offset_y = int(h * 0.2)
 
-            mask = cv2.imread(FILTER_DIR + "mask.png", -1)
-            mask = cv2.resize(mask, (w + offset_x, h + offset_y))
+            mask = process_image(filter, w + offset_x, h + offset_y)
 
-            frame = overlay_image(y - int(0.2*offset_y), x - int(0.5*offset_x), mask, frame)
-        return frame
-    elif filter == "swap":
-        if len(faces_rects) is not 2:
-            return
-        ((lx, ly, lw, lh), (rx, ry, rw, rh)) = faces_rects
-        for i in range(0, lh):
-            for j in range(0, lw):
-                frame[ly + i, lx + j] = frame[ry + i, rx + j]
-        return frame
+            overlay_image(y - int(0.2*offset_y), x - int(0.5*offset_x), mask)
+    elif filter == "santabeard":
+        for (x, y, w, h) in faces:
+            offset_x = int(w * 0.2)
+            offset_y = int(h * 0.2)
+
+            santa_beard = process_image(filter, w + offset_x, h + offset_y)
+
+            overlay_image(y + int(3.0 * offset_y), x - int(0.5 * offset_x), santa_beard)
+    elif filter == "mustache":
+        for (x, y, w, h) in faces:
+            offset_x = int(w * -0.5)
+            offset_y = int(h * -0.5)
+
+            mustache = process_image(filter, w + offset_x, h + offset_y)
+
+            overlay_image(y - int(1.0 * offset_y), x - int(0.5 * offset_x), mustache)
+    elif filter == "eyeballs":
+        for (x, y, w, h) in eyes:
+            offset_x = int(w * -0.1)
+            offset_y = int(h * -0.1)
+
+            mustache = process_image(filter, w + offset_x, h + offset_y)
+
+            overlay_image(y - int(1.0 * offset_y), x - int(0.5 * offset_x), mustache)
 
 
-def overlay_image(offset_y, offset_x, filter, frame):
-    filter_w, filter_h, _ = filter.shape
+def process_image(name, w, h):
+    image = cv2.imread(FILTER_DIR + name + ".png", -1)
+    return cv2.resize(image, (w, h))
+
+
+def overlay_image(offset_y, offset_x, image):
+    filter_w, filter_h, _ = image.shape
     for i in range(0, filter_h):
         for j in range(0, filter_w):
-            if filter[j, i][3] != 0:
-                frame[offset_y + j, offset_x + i] = filter[j, i]
-    return frame
+            #  Check if image is outside of frame
+            if i + offset_x >= frame.shape[1] or j + offset_y >= frame.shape[0]:
+                break
+            if image[j, i][3] != 0:
+                frame[offset_y + j, offset_x + i] = image[j, i]
+
+
+def get_boundaries(rects):
+    if rects[0][0] > rects[1][0]:
+        ((rx, ry, rw, rh), (lx, ly, lw, lh)) = rects
+    else:
+        ((lx, ly, lw, lh), (rx, ry, rw, rh)) = rects
+
+    x = lx
+    y = ly
+    min_w = lw
+    min_h = lh
+    max_w = rx + rw - lx
+    max_h = ly + lh - ry
+
+    return x, y, min_w, min_h, max_w, max_h
+
+
+def show_boundaries(rects, color):
+    for (x, y, w, h) in rects:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 3)
 
 
 def rotate_image(mat, angle):
     height, width = mat.shape[:2]  # image shape has 3 dimensions
-    image_center = (width/2, height/2) # getRotationMatrix2D needs coordinates in reverse order (width, height) compared to shape
+    image_center = (width/2, height/2)
 
-    rotation_mat = cv2.getRotationMatrix2D(image_center, angle, 1.)
+    rotation_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
 
     # rotation calculates the cos and sin, taking absolutes of those.
-    abs_cos = abs(rotation_mat[0,0])
-    abs_sin = abs(rotation_mat[0,1])
+    abs_cos = abs(rotation_mat[0, 0])
+    abs_sin = abs(rotation_mat[0, 1])
 
     # find the new width and height bounds
     bound_w = int(height * abs_sin + width * abs_cos)
@@ -86,9 +122,20 @@ def rotate_image(mat, angle):
     return cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h))
 
 
+def detect_multi_scale(cascade, min_neighbors, min_size):
+    return cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=min_neighbors,
+        minSize=min_size,
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
-    ap.add_argument("-f", "--filter", nargs='+', required=True, help="filter to use [santahat, mask, sunglasses]")
+    ap.add_argument("-f", "--filter", nargs='+', required=True, help="filters to use [santahat, mask, sunglasses]")
+    ap.add_argument("-b", "--boundaries", required=False, action='store_true', help="show boundaries of detected areas (optional)")
     ap.add_argument("-ce", "--cascade_eye", required=False, help="haar cascade for eye detection (optional)")
     ap.add_argument("-cf", "--cascade_face", required=False, help="haar cascade for face detection (optional)")
     args = vars(ap.parse_args())
@@ -96,8 +143,6 @@ if __name__ == "__main__":
     #  Creating a face cascade
     face_cascade_path = CASCADE_DIR + (args["cascade_face"] if args["cascade_face"] else "face.xml")
     eye_cascade_path = CASCADE_DIR + (args["cascade_eye"] if args["cascade_eye"] else "eye.xml")
-
-    print(face_cascade_path)
 
     face_cascade = cv2.CascadeClassifier(face_cascade_path)
     eye_cascade = cv2.CascadeClassifier(eye_cascade_path)
@@ -113,31 +158,21 @@ if __name__ == "__main__":
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
 
-        #  Detect faces in our frame.
-        faces = face_cascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
-
-        #  Detect eyes in our frame.
-        eyes = eye_cascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=10,
-            minSize=(10, 10),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
+        #  Detect eyes and feces in our frame.
+        eyes = detect_multi_scale(eye_cascade, 50, (10, 10))
+        faces = detect_multi_scale(face_cascade, 50, (30, 30))
 
         #  Loop over a list of filters
         for filter in args["filter"]:
-            filter = get_image(filter, faces, eyes, frame)
+            menage_filter(filter)
 
+        # Show boundaries if parameter selected
+        if args["boundaries"]:
+            show_boundaries(eyes, color=(255, 0, 0))
+            show_boundaries(faces, color=(0, 255, 0))
 
         #  Display the resulting frame
-        cv2.imshow('FaceDetection', frame)
+        cv2.imshow('Snapchat filter', cv2.flip(frame, 1))
 
         # 'q' pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
